2613741ab033f02c27975d863b1d969bfaddc288
valerioda <valerio.dandrea@lngs.infn.it>
2025-05-28 11:49:25 +0200

    adding config file

--- daq_scope.py
+++ daq_scope.py
@@ -4,15 +4,10 @@ import time
 from datetime import datetime
 import numpy as np
 from matplotlib import pyplot as plt
+import yaml
 
 from lgdo import lh5, Table, Array, WaveformTable, ArrayOfEqualSizedArrays
 from caen_felib import lib, device, error
-
-dig2_scheme = "dig2"
-dig2_authority = "caendgtz-usb-52696"
-dig2_query = ''
-dig2_path = ''
-dig2_uri = f'{dig2_scheme}://{dig2_authority}/{dig2_path}?{dig2_query}'
 
 def get_new_filename(base_name, timestamp_str):
     return f"{base_name}_{timestamp_str}.lh5"
@@ -20,30 +15,48 @@ def get_new_filename(base_name, timestamp_str):
 def main():
     par = argparse.ArgumentParser(description="Save digitizer data to LH5. Press Ctrl+C during acquisition to stop manually.")
     arg, st = par.add_argument, "store_true"
+    arg("-a", "--dig_address", required=True, help="dig2://caendgtz-usb-52696")
     arg("-o", "--out_file", required=True, help="Base output file name")
-    arg("-nc", "--n_ch", type=int, default=1, help="Number of channels")
-    arg("-rl", "--record_length", type=int, default=4084, help="Record length in samples")
-    arg("-pt", "--pretrigger", type=int, default=2042, help="Pre-trigger in samples")
-    arg("-dc", "--dc_offset", type=str, default="10", help="DC offset percentage")
+    arg("-c", "--config_file", required=True, help="Configuration file name")
     arg("-tt", "--temperature", action=st, help="Save temperature")
     arg("-st", "--software_trigger", action=st, help="Use software trigger")
-    arg("-ms", "--max_size", type=int, default=100, help="Max LH5 file size in MB")
-    arg("-bs", "--buffer_size", type=int, default=100, help="Number of events to buffer before writing")
     arg("-n", "--n_events", type=int, help="Total number of events to acquire")
     arg("-d", "--duration", type=int, help="Maximum acquisition time in seconds")
 
     args = vars(par.parse_args())
+
+    dig_address = args["dig_address"]
     out_file = args["out_file"]
     base_name = out_file.replace(".lh5", "")
-    record_length = args["record_length"]
-    pretrigger = args["pretrigger"]
-    dc_offset = args["dc_offset"]
-    active_ch = args["n_ch"]
+
+    config = {}
+    config_file = args["config_file"]
+    try:
+        with open(config_file, 'r') as f:
+            config = yaml.safe_load(f)
+        print(f"Loaded configuration from {config_file}")
+    except FileNotFoundError:
+        print(f"Error: Configuration file '{config_file}' not found.")
+        print("Please ensure 'config.yaml' is in the same directory as the script, or provide the full path.")
+        sys.exit(1)
+    except yaml.YAMLError as exc:
+        print(f"Error parsing '{config_file}': {exc}")
+        print("Please check your YAML file for syntax errors.")
+        sys.exit(1)
+
+    gen_settings = config.get("general_settings", {})
+
+    record_length = gen_settings.get("record_length", 4084)
+    pretrigger = gen_settings.get("pretrigger", 2042)
+    dc_offset = gen_settings.get("dc_offset", "10")
+    active_ch = gen_settings.get("n_channels", 1)
+    max_file_size_mb = gen_settings.get("max_file_size_mb", 100)
+    max_file_size_bytes = max_file_size_mb * 1024 * 1024
+    buffer_size = gen_settings.get("buffer_size", 100)
+    software_rate = gen_settings.get("software_trigger_rate", 1000)
+
     save_temperature = args["temperature"]
     trig_source = "SwTrg" if args["software_trigger"] else "TrgIn"
-    software_rate = 1000  # Hz if using SwTrg
-    max_file_size = args["max_size"] * 1024 * 1024  # Convert MB to bytes
-    buffer_size = args["buffer_size"]
     total_events = args.get("n_events")
     max_duration = args.get("duration")
 
@@ -68,9 +81,8 @@ def main():
     event_counter = 0
     start_time = time.time()
 
-    with device.connect(dig2_uri) as dig:
+    with device.connect(dig_address) as dig:
         dig.cmd.reset()
-        dig.cmd.cleardata()
 
         fw_type = dig.par.fwtype.value
         fw_ver = dig.par.fpga_fwver.value
@@ -81,7 +93,7 @@ def main():
         adc_n_bits = int(dig.par.adc_nbit.value)
         sampling_period_ns = int(1e3 / adc_samplrate_msps)
         
-        print(f"Sampling rate = {adc_samplrate_msps} MHz, n. bit = {adc_n_bits}, Sampling period = {sampling_period_ns} ns")
+        #print(f"Sampling rate = {adc_samplrate_msps} MHz, n. bit = {adc_n_bits}, Sampling period = {sampling_period_ns} ns")
 
         nch = int(dig.par.NumCh.value)
         dig.par.iolevel.value = "TTL"
@@ -100,18 +112,21 @@ def main():
         dig.cmd.armacquisition()
         dig.cmd.swstartacquisition()
 
+        print("\nStarting acquisition...")
         try:
             while True:
                 if trig_source == "SwTrg":
                     time.sleep(1 / software_rate)
                     dig.cmd.sendswtrigger()
+
                 try:
                     endpoint.read_data(100, data)
                 except error.Error as ex:
-                    if ex.code in (error.ErrorCode.TIMEOUT, error.ErrorCode.STOP):
+                    if ex.code is error.ErrorCode.TIMEOUT:
                         continue
-                    else:
-                        raise ex
+                    if ex.code is error.ErrorCode.STOP:
+                        break
+                    raise ex
 
                 waveform = data[2].value
                 timestamp = data[1].value
@@ -135,7 +150,7 @@ def main():
                     break
 
                 if len(timestamp_buffer[0]) >= buffer_size:
-                    print(f"Writing {current_file}, n.events {event_counter}")
+                    print(f"...writing current file: {current_file}, total events {event_counter}")
                     for ch in range(active_ch):
                         wfs = np.array(waveform_buffer[ch], dtype=np.uint16)
                         ts = np.array(timestamp_buffer[ch], dtype=np.uint64)
@@ -153,7 +168,7 @@ def main():
                         ts_arr = Array(ts, attrs={"datatype": "array<1>{real}", "units": "ADC"})
                         raw_data = Table(col_dict={"waveform": wf, "timestamp": ts_arr})
 
-                        lh5.write(raw_data, name="raw", lh5_file=current_file, wo_mode="append", group=f"ch{ch:03}")
+                        #lh5.write(raw_data, name="raw", lh5_file=current_file, wo_mode="append", group=f"ch{ch:03}")
 
                         waveform_buffer[ch].clear()
                         timestamp_buffer[ch].clear()
@@ -174,7 +189,7 @@ def main():
                         current_file = get_new_filename(base_name, timestamp_str)
 
         except KeyboardInterrupt:
-            dig.cmd.swstopacquisition()
+            #dig.cmd.swstopacquisition()
             print("KeyboardInterrupt received. Stopping acquisition.")
 
         dig.cmd.disarmacquisition()
