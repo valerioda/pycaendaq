0fa17f7931f3a5ff6364a2cfa60b5d57028e78f1
valerioda <valerio.dandrea@lngs.infn.it>
2025-05-30 17:20:52 +0200

    fixing data format, timestamp in unix time

--- daq_scope.py
+++ daq_scope.py
@@ -94,6 +94,7 @@ def main():
     max_file_size_mb = gen_settings.get("max_file_size_mb", 100)
     max_file_size_bytes = max_file_size_mb * 1024 * 1024
     buffer_size = gen_settings.get("buffer_size", 100)
+    interval_stats = gen_settings.get("interval_stats", 100)
     software_rate = gen_settings.get("software_trigger_rate", 1000)
 
     save_temperature = args["temperature"]
@@ -114,29 +115,17 @@ def main():
 
     event_counter = buffer_counter = 0
     start_time = time.time()
+    start_timestamp = time.time_ns()
 
     with device.connect(dig_address) as dig:
         dig.cmd.reset()
 
-        modelname = dig.par.modelname.value
-        fw_type = dig.par.fwtype.value
-        fw_ver = dig.par.fpga_fwver.value
-        tot_channels = int(dig.par.numch.value)
-        adc_samplrate_msps = float(dig.par.adc_samplrate.value)  # in Msps
-        adc_n_bits = int(dig.par.adc_nbit.value)
-        sampling_period_ns = int(1e3 / adc_samplrate_msps)
-        inputrange = dig.par.inputrange.value
-        inputtype = dig.par.inputtype.value
-        print(f"\nDigitizer model: {modelname}, Firmware: {fw_type} v. {fw_ver}")
-        print(f"Sampling rate = {adc_samplrate_msps} MHz\n",
-              f"n. bit = {adc_n_bits}\n",
-              f"Sampling period = {sampling_period_ns} ns\n",
-              f"Input dynamic range = {inputrange} V\n",
-              f"Input type = {inputtype}\n")
+        print_dig_stats(dig)
 
         print(f"--- Applying general digitizer settings ---")
         for param_name, param_value in gen_settings.items():
-            if param_name in ["software_trigger_rate","max_file_size_mb","buffer_size"]:
+            if param_name in ["software_trigger_rate","max_file_size_mb",
+                              "buffer_size","interval_stats"]:
                 continue
             if param_value is None:
                 continue
@@ -144,7 +133,8 @@ def main():
             dig.set_value(f"/par/{param_name}",str(param_value))
         recordlengths = int(gen_settings["recordlengths"])
         acqtriggersource = gen_settings["acqtriggersource"]
-        
+        tot_channels = int(dig.par.numch.value)
+
         for ch in dig.ch:
             ch.par.chenable.value = "FALSE"
         for group_name, group_dict in channel_settings.items():
@@ -161,10 +151,9 @@ def main():
 
         # data format and endpoint
         data_format = [
-            {"name": "EVENT_SIZE", "type": "SIZE_T"},
-            {"name": "TIMESTAMP", "type": "U64"},
+            {"name": "TIMESTAMP_NS", "type": "U64"},
+            {"name": "TRIGGER_ID", "type": "U32"},
             {"name": "WAVEFORM", "type": "U16", "dim": 2, "shape": [tot_channels, recordlengths]},
-            {"name": "WAVEFORM_SIZE", "type": "U64", "dim": 1, "shape": [tot_channels]},
         ]
         endpoint = dig.endpoint["scope"]
         data = endpoint.set_read_data_format(data_format)
@@ -176,10 +165,11 @@ def main():
         temperature_buffer = []
         waveform_buffer = np.full((active_ch_count, buffer_size, recordlengths), 0, dtype=np.uint16)
         timestamp_buffer = np.full((active_ch_count, buffer_size), 0, dtype=np.uint64)
+        ev_number_buffer = np.full((active_ch_count, buffer_size), 0, dtype=np.uint16)
 
         print("\nStarting acquisition...")
         while True:
-            if acqtriggersource == "SwTrg":
+            if "SwTrg" in acqtriggersource:
                 time.sleep(1 / software_rate)
                 dig.cmd.sendswtrigger()
 
@@ -192,8 +182,9 @@ def main():
                     break
                 raise ex
 
+            timestamp = data[0].value
+            trigger_id = data[1].value
             waveform = data[2].value
-            timestamp = data[1].value
 
             if waveform.shape[0] < active_ch_count or waveform.shape[1] != recordlengths:
                 print(f"[WARNING] Invalid waveform shape: {waveform.shape} (expected {active_ch_count} x {recordlengths})")
@@ -201,7 +192,8 @@ def main():
             
             for i, ch in enumerate(channel_list):
                 waveform_buffer[i, buffer_counter, :] = waveform[ch]
-                timestamp_buffer[i, buffer_counter] = np.uint64(timestamp)
+                timestamp_buffer[i, buffer_counter] = np.uint64(start_timestamp+timestamp)
+                ev_number_buffer[i, buffer_counter] = event_counter
 
             if save_temperature:
                 temp_values = [float(dig.get_value(f"/par/{name}")) for name in temp_names]
@@ -210,7 +202,7 @@ def main():
             event_counter += 1
             buffer_counter += 1
             
-            if (event_counter % 100) == 0:
+            if (event_counter % interval_stats) == 0:
                 print_stats(dig, start_time, event_counter)
 
             if buffer_counter >= buffer_size:
@@ -233,8 +225,11 @@ def main():
                             values_units="ADC"
                         )
     
-                        ts_arr = Array(timestamp_buffer[i], attrs={"datatype": "array<1>{real}", "units": "ADC"})
-                        raw_data = Table(col_dict={"waveform": wf, "timestamp": ts_arr})
+                        ts_arr = Array(timestamp_buffer[i], attrs={"datatype": "array<1>{real}", "units": "ns"})
+                        ev_arr = Array(ev_number_buffer[i], attrs={"datatype": "array<1>{real}"})
+                        raw_data = Table(
+                            col_dict={"eventnumber":ev_arr, "timestamp": ts_arr, "waveform": wf}
+                        )
     
                         lh5.write(raw_data, name="raw", lh5_file=current_file, wo_mode="append", group=f"ch{ch:03}")
     
@@ -270,11 +265,34 @@ def main():
 def get_new_filename(base_name, timestamp_str):
     return f"{base_name}_{timestamp_str}.lh5"
 
+def print_dig_stats(dig):
+    modelname = dig.par.modelname.value
+    fw_type = dig.par.fwtype.value
+    fw_ver = dig.par.fpga_fwver.value
+    adc_samplrate_msps = float(dig.par.adc_samplrate.value)  # in Msps
+    adc_n_bits = int(dig.par.adc_nbit.value)
+    sampling_period_ns = int(1e3 / adc_samplrate_msps)
+    inputrange = dig.par.inputrange.value
+    inputtype = dig.par.inputtype.value
+    print(f"\nDigitizer model: {modelname}, Firmware: {fw_type} v. {fw_ver}")
+    print(f"Sampling rate = {adc_samplrate_msps} MHz\n",
+          f"n. bit = {adc_n_bits}\n",
+          f"Sampling period = {sampling_period_ns} ns\n",
+          f"Input dynamic range = {inputrange} V\n",
+          f"Input type = {inputtype}\n")
+
 def print_stats(dig, start_time, counter):
     elapsed = time.time() - start_time
     rate = counter / elapsed / (1024. * 1024.) # MB/s
-    par_stats = ["acquisitionstatus","livetimemonitor","realtimemonitor",
-                 "deadtimemonitor","triggercnt","losttriggercnt"]
+
+    status = decode_status(dig.get_value("/par/acquisitionstatus"))
+    print(f"Acquisition Status:")
+    for key, value in status.items():
+        print(f"  {key:17}: {'ON' if value else 'OFF'}")
+
+    par_stats = ["livetimemonitor","realtimemonitor","deadtimemonitor",
+                 "triggercnt","losttriggercnt"]
+    print(f"Elapsed time {elapsed:.1f} s, n. events: {counter}, readout rate {rate:.1e} MB/s")
     for par in par_stats:
         val = dig.get_value(f'/par/{par}')
         if par in ["livetimemonitor", "realtimemonitor", "deadtimemonitor"]:
@@ -284,8 +302,24 @@ def print_stats(dig, start_time, counter):
         else:
             name = par
         print(f"{name}: {val} ", end='')
-    print(f"Elapsed time {elapsed:.1f} s, n. events: {counter}, readout rate {rate:.1e} MB/s")
+    print()
 
+def decode_status(status):
+    flags = {
+        0: "Armed",
+        1: "Run",
+        2: "Run_mw",
+        3: "Jesd_Clk_Valid",
+        4: "Busy",
+        5: "PreTriggerReady",
+        6: "LicenseFail",
+    }
+    status = int(status)
+    decoded_status = {}
+    for bit, name in flags.items():
+        decoded_status[name] = bool((status >> bit) & 1)
+
+    return decoded_status
 
 if __name__ == "__main__":
     main()
